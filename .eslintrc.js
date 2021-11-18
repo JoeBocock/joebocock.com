6aab0ba679bc8559414dcd7eb4ef6166b70f94f4
Joe <joebocock@gmail.com>
2021-11-18 19:01:14 +0000

    NuxtJS App.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,16 @@
 module.exports = {
-    root: true,
-    env: {
-        node: true,
-    },
-    extends: ['plugin:vue/essential', 'eslint:recommended'],
-    parserOptions: {
-        parser: 'babel-eslint',
-    },
-    rules: {
-        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    },
-};
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: [
+    '@nuxtjs/eslint-config-typescript',
+    'plugin:nuxt/recommended',
+    'prettier'
+  ],
+  plugins: [
+  ],
+  // add your custom rules here
+  rules: {}
+}
