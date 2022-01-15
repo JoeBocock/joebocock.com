8cdebae7eec064507bc9f22ae8222dcfae16e20a
Joe <joebocock@gmail.com>
2022-01-15 10:50:16 +0000

    Screen Handling.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
     <theme-toggle class="absolute right-none top-none margin-medium padding-small"/>
     <div class="fill-space flex flex-column flex-center">
-        <h2 class="title-large">Hey there,</h2>
+        <h2 class="title-large">Hey There,</h2>
         <span class="title-medium">I'm Joe</span>
         <desk-vector-graphic />
-        <div class="flex space-between flex-center space-between" style="width: 26rem">
-            <button class="navigation-button padding-small">who</button>
-            <button class="navigation-button padding-small">what</button>
-            <button class="navigation-button padding-small">work</button>
-        </div>
+        <!-- <div class="width-content flex space-between flex-center space-between text-medium">
+            <button aria-label="Who am I?" class="navigation-button padding-small"><strong>who</strong></button>
+            <button aria-label="What do I enjoy?" class="navigation-button padding-small"><strong>what</strong></button>
+            <button aria-label="Work I have done" class="navigation-button padding-small"><strong>work</strong></button>
+        </div> -->
     </div>
 </template>
 
@@ -18,6 +18,11 @@ import DeskVectorGraphic from './components/svgs/DeskVectorGraphic.vue'
 
 export default {
     name: 'App',
+    data() {
+        return {
+            page: '',
+        }
+    },
     components: {
         DeskVectorGraphic,
         ThemeToggle,
