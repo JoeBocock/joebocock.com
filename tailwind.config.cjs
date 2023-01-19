36b266c4ef3d3e138239356f4d42364e66dfb0a0
Joe Bocock <joebocock@gmail.com>
2023-01-19 20:28:19 +0000

    WIP

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        extend: {
+            colors: {
+                polar: {
+                    1: '#4C566A',
+                    2: '#434C5E',
+                    3: '#3B4252',
+                    4: '#2E3440',
+                },
+                snow: {
+                    1: '#ECEFF4',
+                    2: '#E5E9F0',
+                    3: '#D8DEE9',
+                },
+                frost: {
+                    1: '#8FBCBB',
+                    2: '#88C0D0',
+                    3: '#81A1C1',
+                    4: '#5E81AC',
+                },
+                aurora: {
+                    1: '#BF616A',
+                    2: '#D08770',
+                    3: '#EBCB8B',
+                    4: '#A3BE8C',
+                    5: '#B48EAD',
+                },
+            }
+        },
+    },
+    plugins: [],
 }
