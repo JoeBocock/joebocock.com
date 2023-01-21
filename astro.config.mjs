3edf755544c5e2a6b2055e303363efba7b3efe4a
Joe Bocock <joebocock@gmail.com>
2023-01-21 14:33:59 +0000

    Favicon and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 export default defineConfig({
   markdown: {
     shikiConfig: {
@@ -15,5 +18,6 @@ export default defineConfig({
       wrap: true
     }
   },
-  integrations: [tailwind(), mdx()]
+  site: 'https://joebocock.com',
+  integrations: [tailwind(), mdx(), sitemap()]
 });
